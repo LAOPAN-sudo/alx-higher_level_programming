ef3d10b76823f5c7d3cc37deaf1df052c045f57a
LAOPAN-sudo <laopanpare@protonmail.com>
2023-08-10 16:03:46 -0700

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     """Return sum, difference, product, quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
+    a = 10
+    b = 5
+
     result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
