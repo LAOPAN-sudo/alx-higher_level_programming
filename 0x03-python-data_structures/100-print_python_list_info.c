205fe3130559322972a561a1f60e6636ee5d285e
LAOPAN-sudo <laopanpare@protonmail.com>
2023-08-12 08:03:02 -0700

    14. CPython #0: Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	PyObject *element;
+	Py_ssize_t size, allocated, i;
+
+	size = PyList_Size(p);
+	allocated = list->allocated;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		element = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
+	}
+}
